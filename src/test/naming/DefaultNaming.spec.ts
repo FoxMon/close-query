5f63564cdf3e16cad5e910263ebf0918312f33de
foxmon <jjockrod@naver.com>
2024-01-08 14:10:50 +0900

    naming spec

---
+++ src/test/naming/DefaultNaming.spec.ts
@@ -0,0 +1,12 @@
+import { describe, test, expect } from 'vitest';
+import { DefaultNaming } from '../../naming/DefaultNaming';
+
+describe('DefaultNaming.ts', () => {
+    test('DefaultNaming.getColumnName()', () => {
+        const defaultNaming: DefaultNaming = new DefaultNaming();
+
+        const columnName: string = defaultNaming.getColumnName('article', 'Article', ['hbtb']);
+
+        expect(columnName).toEqual('hbtbArticle');
+    });
+});
